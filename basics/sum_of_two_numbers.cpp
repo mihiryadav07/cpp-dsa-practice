b8a3d327f4c7731957bf5e35556a50b5baeedfd3
Mihir Yadav <mihiryadav.in@gmail.com>
2026-06-25 10:32:18 +0530

    sum_of_two_numbers.cpp

---
+++ basics/sum_of_two_numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () 
+{
+ int a , b;
+ cout << "enter a: " << endl;
+ cin >> a;
+ cout << "enter b: " << endl;
+ cin >> b;
+ cout << "sum of two numbers a+b is: " << a+b << endl;
+    return 0;
+}
