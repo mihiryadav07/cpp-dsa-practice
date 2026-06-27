7edbd260afd4d8c363a4abf652f33767ebc9c2dc
Mihir Yadav <mihiryadav.in@gmail.com>
2026-06-27 23:29:26 +0530

    factorial.cpp

---
+++ loops/factorial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int n ;
+ long long fact = 1;
+ cout<< "enter the n = ";
+ cin >> n;
+ 
+ for(int i =2 ; i<=n; i++)
+ { fact*=i;}
+ cout << "factorial = " << fact << endl;
+    return 0;
+}
