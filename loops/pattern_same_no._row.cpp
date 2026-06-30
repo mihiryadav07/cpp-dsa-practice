2e2ad2e69b21038969acabcfc70be6e4820f3724
Mihir Yadav <mihiryadav.in@gmail.com>
2026-06-30 20:14:17 +0530

    pattern_same_no._row.cpp

--- loops/pattern_same_no._row.cpp
+++ loops/pattern_same_no._row.cpp
@@ -4,15 +4,9 @@ int main () {
 
     int n=4;
      
-     for(int i=0; i<n; i++)
-{    //spaces
-    for(int j=0;j<i;j++)
-    {cout << " ";}
-
-    //num
-    
-    for(int j=0;j<n-i;j++)
-    {cout << i+1 ;} cout << endl;
+     for(int i=1; i<=n; i++)
+{   for(int j=1; j<=n ; j++)
+    {cout << j; }  cout << endl;
 }
     return 0;
 }
