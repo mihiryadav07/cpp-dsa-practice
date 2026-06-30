c6c2536f801490c74893b99a256fd2ae19a97df3
Mihir Yadav <mihiryadav.in@gmail.com>
2026-06-30 20:11:12 +0530

    pattern_continous_nos.cpp

--- loops/pattern_same_no._row.cpp
+++ loops/pattern_same_no._row.cpp
@@ -3,12 +3,16 @@ using namespace std;
 int main () {
 
     int n=4;
+     
+     for(int i=0; i<n; i++)
+{    //spaces
+    for(int j=0;j<i;j++)
+    {cout << " ";}
 
-     for(int i=1; i<=n; i++)
-{    int m = 1;
-     for (int j=1;j<=n;j++)
-     {cout << m ; m++; } 
-     cout << endl;
+    //num
+    
+    for(int j=0;j<n-i;j++)
+    {cout << i+1 ;} cout << endl;
 }
     return 0;
 }
