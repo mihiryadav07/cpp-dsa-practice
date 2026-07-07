cfe9c2436aa1578baf506a3c4da544d8eeba7a61
Mihir Yadav <mihiryadav.in@gmail.com>
2026-07-08 00:20:52 +0530

    pattern_hollow_diamond.cpp

---
+++ loops/pattern_hollow_diamond.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+int main () 
+{
+    int n = 4;
+
+    //TOP
+     for(int i =0; i<n; i++)
+     
+     {  //SPACES
+        for(int j=0; j<n-i-1; j++)
+        {cout << " ";}
+     
+        cout << "*";
+
+        if(i!=0)
+        {   //SPACES
+           
+            for(int j=0; j<2*i-1; j++)
+            {cout << " ";}
+    
+             cout << "*";
+         }
+     
+         cout << endl;
+     
+    }
+
+    //BOTTOM
+    for(int i=0;i<n-1;i++)
+    
+    {  //SPACES
+        for(int j=0; j<i+1; j++)
+        {cout << " ";}
+    
+         cout << "*";
+
+     if(i!=n-2)
+      {  //SPACES
+       
+        for(int j=0; j<2*(n-i)-5; j++)
+        {cout << " ";}
+    
+         cout << "*";
+      }
+
+       cout << endl;
+     }
+ 
+    return 0;
+}
